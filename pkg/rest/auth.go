04f0d60664bc87c64a33ca4ce78f57d5bfc09e64
dnitsch <nitschneiderd@gmail.com>
2022-09-08 13:08:20 +0100

    fix: auth map initial

--- pkg/rest/auth.go
+++ pkg/rest/auth.go
@@ -24,39 +24,71 @@ type ConfigOAuth struct {
 
 // Auth holds the auth strategy for all Seeders
 type AuthConfig struct {
-	AuthStrategy AuthType          `yaml:"type"`
-	Username     string            `yaml:"username"`
-	Password     string            `yaml:"password"`
-	OAuth        *ConfigOAuth      `yaml:"oauth,omitempty"`
-	HttpHeaders  map[string]string `yaml:"httpHeaders,omitempty"`
+	AuthStrategy AuthType     `yaml:"type"`
+	Username     string       `yaml:"username"`
+	Password     string       `yaml:"password"`
+	OAuth        *ConfigOAuth `yaml:"oauth,omitempty"`
 }
 
-// Auth holds the auth strategy for all Seeders
-type Auth struct {
-	config           AuthConfig
-	oAuthConfig      *clientcredentials.Config
-	basicAuthToToken string
+type AuthMap map[string]AuthConfig
+
+// type Auth map[string]struct {
+// 	config           AuthMap
+// 	oAuthConfig      *clientcredentials.Config
+// 	basicAuthToToken string
+// }
+
+// Auth holds the auth strategy for each seeder
+type auth struct {
+	authStrategy AuthType
+	oAuthConfig  *clientcredentials.Config
+	basicAuth    *basicAuth
+	basicToToken *basicToToken
 }
 
-func NewAuth(a AuthConfig) *Auth {
-	ac := &Auth{
-		config: a,
-	}
-	if a.AuthStrategy == OAuth && a.OAuth != nil {
-		c := &clientcredentials.Config{
-			ClientID:       a.Username,
-			ClientSecret:   a.Password,
-			TokenURL:       a.OAuth.ServerUrl,
-			Scopes:         a.OAuth.Scopes,
-			EndpointParams: a.OAuth.EndpointParams,
-			AuthStyle:      oauth2.AuthStyleInParams,
+type basicAuth struct {
+	username string
+	password string
+}
+
+type basicToToken struct {
+	username     string
+	password     string
+	headerKey    string
+	headerValFmt string
+}
+
+type authMap map[string]auth
+
+func NewAuth(am *AuthMap) *authMap {
+	ac := authMap{}
+	for k, v := range *am {
+		a := auth{}
+		a.authStrategy = v.AuthStrategy
+		switch strategy := v.AuthStrategy; strategy {
+		case OAuth:
+			c := &clientcredentials.Config{
+				ClientID:       v.Username,
+				ClientSecret:   v.Password,
+				TokenURL:       v.OAuth.ServerUrl,
+				Scopes:         v.OAuth.Scopes,
+				EndpointParams: v.OAuth.EndpointParams,
+				AuthStyle:      oauth2.AuthStyleInParams,
+			}
+			if v.OAuth.OAuthSendParamsInHeader {
+				c.AuthStyle = oauth2.AuthStyleInHeader
+			}
+			a.oAuthConfig = c
+			ac[k] = a
+		case Basic:
+			a.basicAuth = &basicAuth{username: v.Username, password: v.Password}
+			ac[k] = a
+		default:
+			a.basicAuth = &basicAuth{username: v.Username, password: v.Password}
+			ac[k] = a
 		}
-		if a.OAuth.OAuthSendParamsInHeader {
-			c.AuthStyle = oauth2.AuthStyleInHeader
-		}
-		ac.oAuthConfig = c
 	}
-	return ac
+	return &ac
 }
 
 // NOTE: for oauth an basicAuthToToken it might make sense to build a in-memory map of tokens to strategy name
